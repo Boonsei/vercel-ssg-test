118047c8ffc496039f0cb902118f1997936f1e9e
Boonsei <144961189+Boonsei@users.noreply.github.com>
2026-06-22 10:38:54 +0200

    testing script

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,12 @@
+<script setup>
+console.log("Hallo Vercel");
+
+const counter = ref(0);
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <div>
+        <button @click="counter++">Increment</button>
+        <p>{{ counter }}</p>
+    </div>
 </template>
